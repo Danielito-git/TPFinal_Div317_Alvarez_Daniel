70028c45845b41c91522c59cb7f46ee3af1665da
Danielito-git <daniel.tobias.alv@gmail.com>
2026-07-15 11:56:29 -0300

    Update opciones.py

--- forms/opciones.py
+++ forms/opciones.py
@@ -1,12 +1,18 @@
 import pygame as pg
-import forms.controlador as con
 import confsonido as sonido
 import funciones as fun
 import variablesyconst as var
-import sys
 from utn_fra.pygame_widgets import (
     Label
 )
+
+
+def volver_menu(form_controlador, form_opciones, evento):
+    if evento.type == pg.MOUSEBUTTONDOWN and evento.button == 1:
+        mx, my = evento.pos
+
+        if form_opciones["btn_volver"]["rect"].collidepoint(mx, my):
+            form_controlador["lugar"] = "form_menu"
 
 
 def form_opciones(datos_iniciales):
@@ -48,8 +54,15 @@ def form_opciones(datos_iniciales):
                                                 ancho // 2 - espacio - 60,280,w=60, h=60,
                                     )
     
-    opciones_armado["lbl_volumen"]=  Label(ancho // 2, y=310,text=f'{sonido.get_actual_volume()}', screen=screen,
-                                    font_path=var.FUENTELETRA, font_size=55, color=pg.Color('white'))
+    opciones_armado["lbl_volumen"] = Label(
+        ancho // 2,
+        y=310,
+        text="",
+        screen=screen,
+        font_path=var.FUENTELETRA,
+        font_size=55,
+        color=pg.Color('white')
+    )
     
     opciones_armado["btn_volver"]= fun.crear_boton("Volver", fuente_letra, var.colores["rojo"], var.colores["naranja"],
                                                 ancho // 2 - 100,450, 200, 50
@@ -66,27 +79,47 @@ def form_opciones(datos_iniciales):
 
 
 def dibujar_opciones(form_opciones, form_controlador):
-
     screen = pg.display.get_surface()
     screen.blit(var.OPCIONES_ESCALADO, (0, 0))
     volumen = int(form_controlador["conf_musica"]["volumen"])
-    form_opciones["lbl_volumen"].texto = f"{volumen}%"
+    form_opciones["lbl_volumen"].update_text(f"{volumen}%", pg.Color('white'))
     
     # botones
     for boton in form_opciones["widgets_list"]:
         fun.draw_button(screen, boton)
 
-    # titulos
+    # labels
     form_opciones["lbl_titulo"].draw()
     form_opciones["lbl_volumen"].draw()
 
 
-def volver_menu(form_controlador, form_opciones, evento):
+def manejar_eventos_opciones(datos_iniciales, form_opciones_data, evento):
     if evento.type == pg.MOUSEBUTTONDOWN and evento.button == 1:
         mx, my = evento.pos
 
-        # Si hizo clic en el botón VOLVER
-        if form_opciones["btn_volver"]["rect"].collidepoint(mx, my):
-            form_controlador["lugar"] = "form_menu"
-    
+        if form_opciones_data["btn_musica"]["rect"].collidepoint(mx, my):
+            sonido.mute_desmute(datos_iniciales)
+
+        elif form_opciones_data["btn_mas"]["rect"].collidepoint(mx, my):
+            sonido.modificar_volumen(var.STEP_VOLUME, datos_iniciales)
+
+        elif form_opciones_data["btn_menos"]["rect"].collidepoint(mx, my):
+            sonido.modificar_volumen(-var.STEP_VOLUME, datos_iniciales)
+
+        elif form_opciones_data['btn_volver']['rect'].collidepoint(mx, my):
+            datos_iniciales['lugar'] = 'form_menu'
+
+
+def actualizar_opciones(datos_iniciales, eventos=None):
+    if "form_opciones" not in datos_iniciales:
+        datos_iniciales["form_opciones"] = form_opciones(datos_iniciales)
+
+    form_opciones_data = datos_iniciales["form_opciones"]
+    dibujar_opciones(form_opciones_data, datos_iniciales)
+
+    if eventos:
+        for evento in eventos:
+            manejar_eventos_opciones(datos_iniciales, form_opciones_data, evento)
+
+
     
